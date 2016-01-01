dd8bc2561b0abc1079b8fd681179e5ba35749437
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 01:33:58 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,23 +1,22 @@
-'use strict';
-var test = require('ava');
-Math.imul = undefined;
-var imul = require('./');
+import test from 'ava';
 
-test(function (t) {
-	t.assert(imul(5, -0) === 0);
-	t.assert(imul(0.2, 7) === 0);
-	t.assert(imul(7, 0.9) === 0);
-	t.assert(imul(0.4, 7) === 0);
-	t.assert(imul(-0, 7) === 0);
-	t.assert(imul(8, 0.1) === 0);
-	t.assert(imul(-1, 8) === -8);
-	t.assert(imul(-2, -2) === 4);
-	t.assert(imul(7, 1.9) === 7);
-	t.assert(imul(2, 4) === 8);
-	t.assert(imul(1.9, 7) === 7);
-	t.assert(imul(1.1, 7) === 7);
-	t.assert(imul(7, 1.1) === 7);
-	t.assert(imul(0xfffffffe, 5) === -10);
-	t.assert(imul(0xffffffff, 5) === -5);
-	t.end();
+Math.imul = undefined;
+const m = require('./');
+
+test(t => {
+	t.is(m(5, -0), 0);
+	t.is(m(0.2, 7), 0);
+	t.is(m(7, 0.9), 0);
+	t.is(m(0.4, 7), 0);
+	t.is(m(-0, 7), 0);
+	t.is(m(8, 0.1), 0);
+	t.is(m(-1, 8), -8);
+	t.is(m(-2, -2), 4);
+	t.is(m(7, 1.9), 7);
+	t.is(m(2, 4), 8);
+	t.is(m(1.9, 7), 7);
+	t.is(m(1.1, 7), 7);
+	t.is(m(7, 1.1), 7);
+	t.is(m(0xfffffffe, 5), -10);
+	t.is(m(0xffffffff, 5), -5);
 });
